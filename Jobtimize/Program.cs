61b7203922872bfcac506182304d0dbc30fe6ffb
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-05-01 16:18:42 -0700

    experimenting with looped document creation, editing json files for readability/manipulability

--- Jobtimize/Program.cs
+++ Jobtimize/Program.cs
@@ -1,21 +1,36 @@
-using System.Xml.XPath;
-using System.Net.Http.Headers;
-using System.Data;
-using System.Runtime.CompilerServices;
 using System;
 using System.IO;
+using System.Data;
 using Newtonsoft.Json;
+using System.Xml.XPath;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using Xceed.Words.NET;  
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
 
 class JobItem
 {
     public string Keyword { get; set; }
-    public string Job_description { get ; set; }
+    public string Location { get; set; }
     public string Job_title { get; set; }
     public string Job_link { get; set; }
-    public string Location { get; set; }
     public string Company { get; set; }
+    public string Company_link { get; set; }
+    public string Job_location { get; set; }
+    public string Job_description { get ; set; }
+    public string Seniority_level { get; set; }
+    public string Employment_level { get; set; }
+    public string Job_function { get; set; }
+    public string Industries { get; set; }
+    public string Person_hiring { get; set; }
 }
-class Program
+class Program 
 {
     static void Main(string[] args)
     {
@@ -37,7 +52,7 @@ class Program
             Console.WriteLine(item);
         }
 
-        string jsonFilePath = "ScrapedData/JobData.json";
+        string jsonFilePath = "modified_JobData.json";
         string jsonData = File.ReadAllText(jsonFilePath);
         List<JobItem> jobItems = JsonConvert.DeserializeObject<List<JobItem>>(jsonData);
 
@@ -45,7 +60,30 @@ class Program
         // Iterate through each item in the list and print the 'Job_description' property to the console, check if any of the skills in the ExistingSkills.csv file exist in the job description, and if they do, write that to the console
         foreach (JobItem item in jobItems)
         {
-            // Console.WriteLine($"Job description: {item.Job_description}");
+            
+            string modifiedJson = JsonConvert.SerializeObject(jobItems, Formatting.Indented);
+            string folderPath = "CreatedFiles";
+            string fileName = ($"{item.Company} - {item.Location} - {item.Job_title}");  
+            string outputPath = Path.Combine(folderPath, fileName + ".docx");
+            var document = DocX.Create(outputPath);  
+            document.InsertParagraph("Here are the Applicant's Skills:");
+            foreach (string skill in existingSkills)
+            {
+                if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
+                {
+                document.InsertParagraph(skill);
+                }
+            }
+            document.InsertParagraph("This is the Job Description");
+            document.InsertParagraph(
+                item.Company + " " + 
+                item.Location + " " + 
+                item.Job_title + " " + 
+                item.Job_description);
+            document.Save();  
+
+
+
             Console.WriteLine($"Company: {item.Company}   Title: {item.Job_title}    Location: {item.Location}    Job Description: {item.Job_description}");
             foreach (string skill in existingSkills)
             {
@@ -55,7 +93,8 @@ class Program
                 }
             }
 
-            Console.WriteLine(); // Add an empty line for better readability
+            Console.WriteLine(); 
+            // Add an empty line for better readability
         }
     }
 }
