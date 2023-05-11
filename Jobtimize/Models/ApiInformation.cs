d66a5cc371d3110c6a542bd3cac488009eb0d058
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-05-11 12:50:42 -0700

    refactoring, deciding on best practices

--- Jobtimize/Models/ApiInformation.cs
+++ Jobtimize/Models/ApiInformation.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using ConsoleApiCall.Keys;
+using Jobtimize.Models;
 
 namespace Jobtimize.Models
 {
@@ -7,13 +8,13 @@ namespace Jobtimize.Models
     public class ApiInformation
     {
         public static string ApiUrl = "https://api.openai.com/v1/completions";
-        public string ApiKey = EnvironmentalVariables.ApiKey;
-        public static string IncompletePrompt = $"write me a cover letter for the following job description as though you were a recent coding bootcamp graduate.  Do not claim that I have more than 1 year of experience.  Assume you haveChoose from the following list of skills when you write the cover letter and only include the ones most applicable to the job description:  HTML, CSS, JavaScript, SQL, C#, .NET, MVC, React, Razor, Razor Framework, EF Core, Entity Framework Core, Git, Github, Node.js, Object Oriented Programming, Test-Driven Development, Asynchrony, calling APIs, creating APIs, Authentication with Identity, Authorization, Canvas Methods in JavaScript, Redux, NoSQL, Functional Programming, Bootstrap, Markdown, ES6, ECMAscript.  Be sure to only include skills that are contained within the job description, and do not claim to have any skills that aren't listed above. Only return to me the text for the cover letter.  Here is the job description: ";
+        public static string ApiKey = EnvironmentalVariables.ApiKey;
+        public static string IncompletePrompt = $"write me a cover letter for the following job description as though you were a recent coding bootcamp graduate.  Do not claim that I have more than 1 year of experience.  Assume you haveChoose from the following list of skills when you write the cover letter and only include the ones most applicable to the job description:  HTML, CSS, JavaScript, SQL, C#, .NET, MVC, React, Razor, Razor Framework, EF Core, Entity Framework Core, Git, Github, Node.js, Object Oriented Programming, Test-Driven Development, Asynchrony, calling APIs, creating APIs, Authentication with Identity, Authorization, Canvas Methods in JavaScript, Redux, NoSQL, Functional Programming, Bootstrap, Markdown, ES6, ECMAscript.  Be sure to only include skills that are contained within the job description, and do not claim to have any skills that aren't listed above. Only return to me the text for the cover letter.  Here is the job description:";
         public static string ModelName = "text-davinci-003";
 
         public static Dictionary<string, object> RequestBodyDict = new Dictionary<string, object>  
                             {
-                        {"prompt", prompt}, 
+                        {"prompt", IncompletePrompt}, 
                         {"max_tokens", 50 },
                         {"model", ModelName },
                         {"temperature", 0.5},
--- Jobtimize/Program.cs
+++ Jobtimize/Program.cs
@@ -21,112 +21,103 @@ namespace Jobtimize
 {
     class Program 
     {
+    private static string CreateFolderPath(JobItem item)
+    {
+    return $"CreatedFiles/{item.Company} - {item.Location} - {item.Job_title}";
+    }
+    private static string CreateResumeName(JobItem item)
+    {
+        return $"Resume - {item.Company} - {item.Location} - {item.Job_title}";
+    }
+    private static string CreateCoverLetterName(JobItem item)
+            {
+                return $"Cover Letter - {item.Company} - {item.Location} - {item.Job_title}";
+            }
+        
         static async Task Main(string[] args)
         {
-            //this is the api thingy
-            HttpClient httpClient = new HttpClient();
+        HttpClient httpClient = new HttpClient();
 
-            // Iterate through each item in the list and print the 'Job_description' property to the console, check if any of the skills in the ExistingSkills.csv file exist in the job description, and if they do, write that to the console
+        foreach (JobItem item in JobItemParameters.JobItems)
+        {
+            int numberOfSkills = 0;
+            
+            foreach (string skill in ExistingSkills.List)
+                if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
+                {
+                    numberOfSkills += 1;
+                }
+                if (numberOfSkills >=5)
+                {
 
-            foreach (JobItem item in JobItemParameters.JobItems)
-            {
-                int numberOfSkills = 0;
+                var documentResume = DocX.Create(Path.Combine(CreateFolderPath(item), CreateResumeName(item) + ".docx"));  
+                var documentCoverLetter = DocX.Create(Path.Combine(CreateFolderPath(item), CreateCoverLetterName(item) + ".docx"));
+
+
+
+                ApiInformation.RequestBodyDict["prompt"] = ApiInformation.RequestBodyDict["prompt"] += $"{item.Job_description}";
+                Console.WriteLine(ApiInformation.RequestBodyDict["prompt"]);
+
+
+
+
+                // create the HTTP request message
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, ApiInformation.ApiUrl);
+
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", ApiInformation.ApiKey);
+
+                request.Content = new StringContent(JsonConvert.SerializeObject(ApiInformation.RequestBodyDict), System.Text.Encoding.UTF8, "application/json");
+            
+                // send the HTTP request
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+            
+                // read the response content as a string
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<ChatGptResponse>(responseContent);
+                string responseText = responseObject.choices[0].text;
+                // display the response content
+
+                Console.WriteLine($"ChatGPT response for the prompt: {responseContent}");
+
+
+                Directory.CreateDirectory(CreateFolderPath(item));
+                
+                documentResume.InsertParagraph("Here are the Applicant's Skills:");
                 foreach (string skill in ExistingSkills.List)
+                {
                     if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
                     {
-                        numberOfSkills += 1;
+                    documentResume.InsertParagraph(skill);
                     }
-                    if (numberOfSkills >=5)
+                }
+                documentResume.InsertParagraph("This is the Job Description");
+                documentResume.InsertParagraph(
+                    item.Company + " " + 
+                    item.Location + " " + 
+                    item.Job_title + " " + 
+                    item.Company_link + " " +
+                    item.Job_link + " " +
+                    item.Job_description);
+                documentResume.Save();  
+
+
+                documentCoverLetter.InsertParagraph(responseText);
+                documentCoverLetter.Save();
+
+
+                Console.WriteLine($"Company: {item.Company}   Title: {item.Job_title}    Location: {item.Location}    Job Description: {item.Job_description}");
+                foreach (string skill in ExistingSkills.List)
+                {
+                    if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
                     {
-                    string folderPath = ($"CreatedFiles/{item.Company} - {item.Location} - {item.Job_title}");
-                    string modifiedJson = JsonConvert.SerializeObject(JobItemParameters.JobItems, Formatting.Indented);
-
-
-                    string fileName = ($"Resume - {item.Company} - {item.Location} - {item.Job_title}");  
-                    string outputPath = Path.Combine(folderPath, fileName + ".docx");
-                    var document = DocX.Create(outputPath);  
-                    
-                    string fileNameGPTResponse = ($"Cover Letter - {item.Company} - {item.Location} - {item.Job_title}");
-                    string gptOutputPath = Path.Combine(folderPath, fileNameGPTResponse + ".docx");
-                    var documentGPTResponse = DocX.Create(gptOutputPath);
-
-
-
-
-
-                    string CombinedPrompt = ApiInformation.prompt + $"{item.Job_description}";
-
-                    var requestBodyDict = new Dictionary<string, object>()
-                    {
-                        {"prompt", prompt}, 
-                        {"max_tokens", 50 },
-                        {"model", modelName },
-                        {"temperature", 0.5},
-                        {"top_p", 1},
-                        {"frequency_penalty", 0},
-                        {"presence_penalty", 0},
-                        {"stop", null}
-                    };
-
-
-                    // create the HTTP request message
-                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, ApiInformation.ApiUrl);
-
-                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
-
-                    request.Content = new StringContent(JsonConvert.SerializeObject(ApiInformation.RequestBodyDict), System.Text.Encoding.UTF8, "application/json");
+                        Console.WriteLine($"Skill found: {skill}");
+                    }
+                }
+                Console.WriteLine(item.Seniority_level);
                 
-                    // send the HTTP request
-                    HttpResponseMessage response = await httpClient.SendAsync(request);
-                
-                    // read the response content as a string
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    var responseObject = JsonConvert.DeserializeObject<ChatGptResponse>(responseContent);
-                    string responseText = responseObject.choices[0].text;
-                    // display the response content
-
-                    Console.WriteLine($"ChatGPT response for {prompt}: {responseContent}");
-
-
-
-                    Directory.CreateDirectory(folderPath);
-                    
-                    document.InsertParagraph("Here are the Applicant's Skills:");
-                    foreach (string skill in ExistingSkills.List)
-                    {
-                        if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
-                        {
-                        document.InsertParagraph(skill);
-                        }
-                    }
-                    document.InsertParagraph("This is the Job Description");
-                    document.InsertParagraph(
-                        item.Company + " " + 
-                        item.Location + " " + 
-                        item.Job_title + " " + 
-                        item.Company_link + " " +
-                        item.Job_link + " " +
-                        item.Job_description);
-                    document.Save();  
-
-
-                    documentGPTResponse.InsertParagraph("this is chatGPT's Response" + responseText);
-                    documentGPTResponse.Save();
-
-
-                    Console.WriteLine($"Company: {item.Company}   Title: {item.Job_title}    Location: {item.Location}    Job Description: {item.Job_description}");
-                    foreach (string skill in ExistingSkills.List)
-                    {
-                        if (item.Job_description.Contains(skill, StringComparison.OrdinalIgnoreCase))
-                        {
-                            Console.WriteLine($"Skill found: {skill}");
-                        }
-                    }
-                    Console.WriteLine(item.Seniority_level);
-                    
-                    Thread.Sleep(3000);
-                    Console.WriteLine(); 
-                    // Add an empty line for better readability
+                Thread.Sleep(3000);
+                Console.WriteLine(); 
+                // Add an empty line for better readability
                 }
                 
             }
